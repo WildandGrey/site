2011643572abe49cae3c0db62e343c95ce314eb9
claudiovallejo <hola@claudiovallejo.mx>
2017-05-13 19:03:50 -0400

    Initializing Middleman project.

---
+++ config.rb
@@ -0,0 +1,15 @@
+# General
+
+## Directory Indexing
+activate :directory_indexes
+
+## Browser Prefixing
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+## Layouts
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+page '/index.html', layout: 'main'
