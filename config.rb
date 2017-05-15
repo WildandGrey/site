9a1c7049231ae3c4a7786ed0d53077eff4ac3da4
claudiovallejo <hola@claudiovallejo.mx>
2017-05-15 19:08:09 -0400

    Added case-study template layout; created case-study proxies.

--- config.rb
+++ config.rb
@@ -14,3 +14,11 @@ page '/*.json', layout: false
 page '/*.txt', layout: false
 page '/index.html', layout: 'main'
 page '/about.html', layout: 'main'
+page '/case-study.html', layout: 'main'
+
+# Proxies
+
+## Case Studies
+data.case_studies.each do |id, case_study|
+  proxy "/case-studies/#{ case_study.card.title.parameterize }/index.html", "/case-study.html", :locals => { :case_study => case_study }, :ignore => true
+end
