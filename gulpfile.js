ca33aabd5bf1aa1888e0ff6a64adfcccb3f85d4b
claudiovallejo <hola@claudiovallejo.mx>
2017-06-06 07:21:11 -0400

    Added uncss script to gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
 var concatcss = require('gulp-concat-css');
 var cssmin = require('gulp-cssmin');
+var uncss = require('gulp-uncss');
 var gzip = require('gulp-gzip');
 var clean = require('gulp-clean');
 var htmlmin = require('gulp-htmlmin');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var runSequence = require('run-sequence');
+
+//  Remove unused CSS selectors from site.css
+gulp.task('clean-css', function(){
+  return gulp.src('build/stylesheets/site.css')
+    .pipe(uncss({
+      html: ['build/index.html', 'build/about.html', 'build/case-studies/**/*.html']
+    }))
+    .pipe(gulp.dest('build/stylesheets/'));
+});
 
 //  Concatenate stylesheets -> site.css
 gulp.task('concat-css', function (){
@@ -72,7 +82,7 @@ gulp.task('compress-images', function (){
 
 // Run tasks in order
 gulp.task('sequence', function(callback){
-  runSequence('concat-css', 'minify-css', 'compress-css', 'ignore-css', 'minify-html', 'compress-html', 'minify-js', 'compress-js', 'compress-images');
+  runSequence('clean-css', 'concat-css', 'minify-css', 'compress-css', 'ignore-css', 'minify-html', 'compress-html', 'minify-js', 'compress-js', 'compress-images');
 });
 
 // Build
